52f1e8489d7c041ee14779ec7f4a7796fe60f5db
Pedro Teixeira <40210465@esmad.ipp.pt>
2022-09-29 16:56:31 +0100

    added app demo for better undars

---
+++ VUE/Lifecycle Hooks - practice exercises/life_cycle_hooks/src/App.vue
@@ -0,0 +1,33 @@
+
+<!-- SUMMARY -->
+
+  <!-- LIFE CYCLE HOOKS -->
+<!--COMPONENT LIFE CYCLE HOOKS  -->
+
+<template>
+
+<!-- INSIDE THE CHILD ELEMENT WE CREATE THE COMPONENTS AND WE GOING TO WORK WITH LIFE CYCLE HOOKS ON THOSE COMPONENTS-->
+  <div>
+    
+    <!-- HOMEPAGE COMPONENT -->
+    <HomePage/>
+    <!-- COMPONENT FOR THE LOGIN FORM -->
+    <Login-form/>
+
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+
+</script>
+
+<style>
+
+
+
+</style>
