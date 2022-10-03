27e13ba8c946ffacb7e2c3521d5ff6c1192e3726
Pedro Teixeira <40210465@esmad.ipp.pt>
2022-10-03 17:57:02 +0100

    vue update

---
+++ VUE/template_conditionals_loops_events_app/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+
+
+<Profile_info/>
+
+
+</template>
+
+
+<script>
+
+import Profile_info from './components/Profile_info.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Profile_info
+},
+
+// key data to return data inside our application where his mounted
+data(){
+  return{
+    followers : 0,
+  // CREATE USER OBJECT ABOUT THE INFORMATION OF THE USERS
+    user:{
+      id: 1,
+      userName: 'Pedromst2000',
+      firstName:'Pedro',
+      lastName : 'Teixeira',
+      age: 21,
+      email : 'pedromst2000@gmail.com',
+      // TO CHECK IF HIS THE ADMIN OF THE APPLICATION 
+      isAdmin: true
+    }
+  }
+},
+
+// key methods to trigger events handlers
+methods: {
+    
+},
+
+}
+
+
+
+</script>
+
+<style>
+
+</style>
---
+++ VUE/template_conditionals_loops_events_app/src/components/Profile_info.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="profile">
+        
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    name:'Profile_info',
+    props:{
+        msg: String
+    },
+}
+</script>
+
+<style>
+    
+</style>
