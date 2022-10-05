7a312670438b91aa25a24514c4ac0b67fac04131
Pedro Teixeira <40210465@esmad.ipp.pt>
2022-10-05 17:58:53 +0100

    VUE update

--- VUE/VUE-APP-TUTORIAL/vue-app-tutorial/src/App.vue
+++ VUE/VUE-APP-TUTORIAL/vue-app-tutorial/src/App.vue
@@ -4,132 +4,22 @@
 <!-- HTML -->
 <template>
 
-<div id="app-container">
-<div class="info_user">
- @{{ this.user.userName }} - {{fullName}}
-      <span class="followers"> <strong>Followers :</strong>{{this.followers}}</span>&ensp;
-    </div>
-  <!-- WE CAN TRIGGER EVENTS IN TWO WAYS : @eventName="method_property" OR v-on:eventName="method_property" -->
-    <div class="buttons-container">
-  <button id="Follow"  @click="Follow">Follow</button>
-  <button id="changeUser" @click="changeUsername">Change Username</button>
-  &ensp;
-</div>
-  <!-- <button disabled id="disabled" @click="Unfollow">Unfollow</button> -->
-
-</div>  
+<userProfile/>
 
 </template>
 
 <!-- JS -->
 <script>
 
-
-
+import userProfile from './components/userProfile.vue'
 
 // ALL THE DATA FROM THE OBJECT WILL BE EXPORTED/RETURNED INTO THE TEMPLATE ABOVE
 export default {
   
   name: 'App',
-
-// key data
-// to return some data inside our application
-  data(){
-  return{
-    followers : 0,
-  // CREATE USER OBJECT ABOUT THE INFORMATION OF THE USERS
-    user:{
-      id: 1,
-      userName: 'Pedromst2000',
-      firstName:'Pedro',
-      lastName : 'Teixeira',
-      age: 21,
-      email : 'pedromst2000@gmail.com',
-      // TO CHECK IF HIS THE ADMIN OF THE APPLICATION 
-      isAdmin: true
-    }
-  }
-},
+  components:{ userProfile }
 
 
-
-  // WHEN SOME DATA FROM THE OBJECT CHANGES THE FUNCTION WILL RUN INSIDE THE WATCH
-  // WATCH IF SOMETHING WAS CHANGE ? IFtrue console.log('The user was change') : IFfalse console.log('The user is the current')
-  watch:{
-    followers(newfollower, oldfollower){
-      if(oldfollower < newfollower){
-        console.log(`${this.user.userName} was received on follower !!`);
-      }
-    }
-  },
-
-  
-// KEY computed
-  // COMPUTED CACHING VS METHODS properties
-  //COMPUTED properties are cached on their reactive dependencies. A computed property will only re-evaluate when some of its reactive dependices have changed
-  // COMPUTED properties
-  computed:{
-    // CREATE COMPUTED PROPERTY FULLNAME
-    fullName(){
-      // USING 'this' TO REFERENCE THE OBJECT ABOUT THE key Data Above => data()
-      // will return this data when calling the property in the template => {{ fullName }}
-      return `${this.user.firstName} ${this.user.lastName}`
-    }
-  },
-  // KEY methods
-  // METHODS FOR EVENTS HANDLING
-  // Methods properties will handle Events on the DOM like for instance ONclick EVENTS
-  // WE USE METHODS PROPERTIES FOR EVENTS HANDLERS AND WE CALL THEM INSIDE V-DIRECTIVES => @click="method_property_name" or v-on:click
-  // IN ORDER TO TRIGGER methods properties we need events such as onclick events for instance
-  methods: {
-    Follow(){
-      let followbtn = document.getElementById('Follow');
-      if(followbtn.innerText == 'Follow'){
-        this.followers++;
-        followbtn.innerText = 'Unfollow' 
-      }else{
-        this.followers--;
-        followbtn.innerText = 'Follow' 
-      }
-      
-    },
-    // Unfollow(){
-    //   this.followers--
-    //   if(this.followers === 0){
-    //     document.getElementById('disabled').setAttribute('disabled','disabled')
-    //   }
-    //   else{
-    //     document.getElementById('disabled').removeAttribute('disabled','disabled')
-    //   }
-    // }
-
-    changeUsername(){
-     let checkChangedUserName = prompt('Change the username')
-      
-     if(checkChangedUserName === null || checkChangedUserName === ''){
-      this.user.userName === this.user.userName
-      console.log(this.user);
-    } 
-    else{
-      this.user.userName = checkChangedUserName
-      console.log(this.user);
-      alert(`The username was changed to ${this.user.userName}`)
-    }
-
-    }
-  },
-
-  // LIFE CICLE HOOK
-  // EXAMPLE OF LIFE CYCLE HOOK OF A COMPONENT WITH MOUNT PHASE 
-  //key Mounted => can be used to run code after the component has finished the initial rendering and created the DOM node
-  // WHEN TO COMPONENT HIS MOUNTED WILL TRIGGER THIS FUNCTION
-  mounted(){
-    this.Follow()
-  },
-  // WILL TRIGGER WHEN SOME DATA ON THE COMPONENT IS CHANGED/UPDATING
-  updated(){
-    console.log('was updated');
-  },
   // ----------------------------------------------------------------------------------------------------------------------------
 }
 
@@ -138,8 +28,8 @@ export default {
 
 <!-- CSS -->
 <style>
-  /* GLOBAL STYLE FOR THE ALL APPLICATION */
-#app {
+
+  #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -152,23 +42,6 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-}
-
-
-.info_user{
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 0.5em;
-}
-
-.buttons-container{
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 1em;
 }
 
 </style>
