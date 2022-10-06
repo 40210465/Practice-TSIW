7851f8cfcf94c4601d915ac7c1899160b7bd95ac
Pedro Teixeira <40210465@esmad.ipp.pt>
2022-10-07 00:16:57 +0100

    update SQL

--- SQL/MAIN-SQL/DOCUMENTATION/ALL_ANY_SOME operators.sql
+++ SQL/MAIN-SQL/DOCUMENTATION/ALL_ANY_SOME operators.sql
@@ -1,9 +1,41 @@
 -- OPERADORES LÓGICOS
 -- ALL ANY/SOME
 
--- RETURN ONLY THE EMPLOYEES FROM THE 40 DEPARTMENT WHO HAVES A BIGGEST SALARY THEN ALL THE 
+-- RETURN THE EMPLOYEE FROM THE 40 DEPARTMENT WHO HAVES THE BIGGEST SALARY THEN ALL THE 
 -- EMPLOYEES FROM THE 30 DEPARTMENT
 
--- WITH ALL + SUBQUERY
+-- ALL
 
--- WITH > + SUBQUERY
+-- WITH ALL + SUBQUERY
+SELECT * FROM clientes
+WHERE Departamento_Codigo = 40 -- selecting the employees from the 40th department
+AND Salario > ALL (SELECT Salario -- and the salary bying superior then all salaries from the 30th department
+				   FROM   clientes 
+				WHERE Departamento_Codigo = 30);
+
+-- --------------------------------------------------------------------------------------------
+-- WITH MAX AGGRE FUNCTION
+SELECT * FROM clientes 
+WHERE Departamento_Codigo = 40 -- selecting the employees from the 40th department
+AND Salario > (SELECT MAX(Salario) -- and the salary bying superior then the maximum salary from the 30th department
+				   FROM   clientes 
+				WHERE Departamento_Codigo = 30);
+-- ---------------------------------------------------------------------------------------
+SELECT * FROM clientes
+WHERE Departamento_Codigo = 40 
+AND Salario > SOME (SELECT Salario 
+				   FROM   clientes 
+				WHERE Departamento_Codigo = 30);
+                
+-- Now we are selecting the employees/clients who haves superior salary then some employees from the 30th department
+                
+SELECT * FROM clientes
+WHERE Departamento_Codigo = 40 
+AND Salario > (SELECT MIN(Salario) 
+				   FROM   clientes 
+				WHERE Departamento_Codigo = 30);
+                
+                -- WILL RETURN TWO CLIENTS BECAUSE THEY HAVE A SUPERIOR SALARY THEN THE MINIMUM FROM THE 30TH DEPARTMENT
+               
+               -- -----------------------------------------------------------------------------------------------
+                
