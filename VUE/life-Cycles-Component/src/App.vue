a8d5090c56352fd950751656a0cf7dc5e1d0bc38
Pedro Teixeira <40210465@esmad.ipp.pt>
2022-10-12 23:12:21 +0100

    update

---
+++ VUE/life-Cycles-Component/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <LifeCycles v-if="existsComponent"/>
+
+</template>
+
+<script>
+import LifeCycles from './components/lifeCycles.vue';
+
+  export default {
+    components: { LifeCycles },
+
+    data() {
+      return {
+        // if is true the component will show on the DOM
+        // if is false the component will be removed from the DOM
+        existsComponent: true
+      }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ VUE/life-Cycles-Component/src/components/lifeCycles.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        {{ msg }}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: "Hello Vue3"
+            }
+        },
+        // lifecycleHooks
+        // before create component
+        beforeCreate(){
+            console.log("beforeCreate");
+        },
+        // when the component is created
+        created() {
+        console.log("created");           
+        },
+        // before mounting the component
+        beforeMount(){
+            console.log("beforeMount");
+        },
+        // when the component is mounted with the creation of the app
+        mounted() {
+        console.log("mounted");  
+        },
+        // before being updated the component
+        beforeUpdate(){
+            console.log("beforeUpdate");
+        },
+        // this phase will trigger when the some data changes in the componnet
+        updated(){
+            console.log("updated");
+        },
+        // will trigger before the component is removed from the DOM
+        beforeUnmount(){
+            console.log("beforeUnmount");
+        },
+        // will trigger when the component is removed from the DOM
+        unmounted(){
+            console.log("unmounted");
+        }
+    }
+</script>
+
+<style scoped>
+div{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
