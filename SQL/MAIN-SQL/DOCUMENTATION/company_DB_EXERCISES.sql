0700e7800af8f150edb1822b73358643a764c850
Pedro Teixeira <40210465@esmad.ipp.pt>
2022-10-14 02:17:49 +0100

    SQL update

--- SQL/MAIN-SQL/DOCUMENTATION/company_DB_EXERCISES.sql
+++ SQL/MAIN-SQL/DOCUMENTATION/company_DB_EXERCISES.sql
@@ -1,4 +1,4 @@
-- company Database Exercises
+-- company Database Exercises
 -- create the entities for the company database
 
 -- entities : Employee, Branch, Client, Works_With, Branch_Supplier
@@ -9,7 +9,7 @@ first_name VARCHAR(255) NOT NULL,
 last_name VARCHAR(255) NOT NULL,
 birth_date DATE NOT NULL,
 sex VARCHAR(255) NOT NULL,
-salary INT NOT NULL,
+salary FLOAT NULL,
 supervisor_id INT,
 branch_id INT NOT NULL,
 -- constrains :
@@ -19,4 +19,40 @@ constraint `FK_super_id` FOREIGN KEY(supervisor_id) REFERENCES Employee(emp_id),
 constraint `FK_branch_id` FOREIGN KEY(branch_id) REFERENCES Branch(branch_id)
 );
 
--- 
+--
+CREATE TABLE Branch(
+branch_id INT NOT NULL,
+branch_name VARCHAR(255) NOT NULL,
+manager_id INT NOT NULL,
+manager_start_date DATE NOT NULL,
+PRIMARY KEY(branch_id),
+constraint `FK_mgr_id` FOREIGN KEY(manager_id) REFERENCES Employee(emp_id)
+); 
+
+CREATE TABLE `Client`(
+client_id INT NOT NULL,
+client_name VARCHAR(255) NOT NULL,
+branch_id INT NOT NULL,
+PRIMARY KEY(client_id),
+constraint `FK_branch_id` FOREIGN KEY(branch_id) REFERENCES Branch(branch_id)
+); 
+
+CREATE TABLE `Works_With`(
+emp_id INT NOT NULL,
+client_id INT NOT NULL,
+total_sales FLOAT NOT NULL,
+PRIMARY KEY(emp_id, client_id),
+constraint `FK_emp_id` 		FOREIGN KEY(emp_id) REFERENCES Employee(emp_id),
+constraint `FK_client_id`	FOREIGN KEY(client_id) REFERENCES `Client`(client_id)
+);
+
+CREATE TABLE `Branch_Supplier`(
+branch_id INT NOT NULL,
+supplier_name VARCHAR(255) NOT NULL,
+supply_type VARCHAR(255) NOT NULL,
+PRIMARY KEY(branch_id, supplier_name),
+constraint `FK_branch_id` FOREIGN KEY(branch_id) REFERENCES Branch(branch_id)
+);
+
+
+
